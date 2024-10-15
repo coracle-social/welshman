951e5496cf43af2ca19c7d7c3ef1806978440a55
Jon Staab <shtaab@gmail.com>
2024-10-15 09:48:05 -0700

    Bump versions

--- install.sh
+++ install.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-for upstream in $(./get_packages.py); do
-  version=$(sed -nr 's/ +"version": "(.+)",/\1/p' packages/$upstream/package.json)
+upstream=$1
 
-  for downstream in $(./get_packages.py); do
-    n=@welshman/$upstream
-    f=packages/$downstream/package.json
-    v=$(jq '.dependencies["'$n'"] // empty' $f)
+if [[ -z $upstream ]]; then
+  echo "Please provide an upstream package name"
+  die 1
+fi
 
-    if [[ ! -z $v ]]; then
-      jq '.dependencies["'$n'"]="'~$version'"' $f > $f.tmp
-      mv $f.tmp $f
-      mkdir -p packages/$downstream/node_modules/@welshman
-    fi
-  done
+version=$(sed -nr 's/ +"version": "(.+)",/\1/p' packages/$upstream/package.json)
+
+for downstream in $(./get_packages.py); do
+  n=@welshman/$upstream
+  f=packages/$downstream/package.json
+  v=$(jq '.dependencies["'$n'"] // empty' $f)
+
+  if [[ ! -z $v ]]; then
+    jq '.dependencies["'$n'"]="'~$version'"' $f > $f.tmp
+    mv $f.tmp $f
+    mkdir -p packages/$downstream/node_modules/@welshman
+  fi
 done
 
 npm i
