ff7340f326697fc14ebe84852729cbcb37f601d4
Jon Staab <shtaab@gmail.com>
2024-11-02 07:33:05 -0700

    Bump versions

--- tag.sh
+++ tag.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+status=$(git status | grep "nothing to commit")
+
+if [[ -z "$status" ]]; then
+  echo "Can't tag with uncommitted changes"
+  exit 1
+fi
+
 for pkg in $(./get_packages.py); do
   version=$(sed -nr 's/ +"version": "(.+)",/\1/p' packages/$pkg/package.json)
-  status=$(git status | grep "nothing to commit")
-
-  if [[ -z "$status" ]]; then
-    echo "Can't tag with uncommitted changes"
-    exit 1
-  fi
 
   git tag "$pkg/$version" >/dev/null 2>&1
 done
