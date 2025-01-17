26d606878bc2d55cee1cd1224ca3898fd76d9934
Ticruz <thib.duchene@gmail.com>
2025-01-17 16:53:37 +0100

    build ancestor tags with nip-10 or nip-22

--- packages/app/src/tags.ts
+++ packages/app/src/tags.ts
@@ -5,6 +5,7 @@ import {
   getAncestorTags,
   getPubkeyTagValues,
   getIdAndAddress,
+  isReplaceableKind,
 } from "@welshman/util"
 import type {TrustedEvent} from "@welshman/util"
 import {displayProfileByPubkey} from "./profiles.js"
@@ -52,42 +53,64 @@ export const tagReplyTo = (event: TrustedEvent) => {
     }
   }
 
-  // Based on NIP 10 legacy tags, order is root, mentions, reply
-  const {roots, replies, mentions} = getAncestorTags(event.tags)
+  if (event.kind === 1) {
+    // Based on NIP 10 legacy tags, order is root, mentions, reply
+    const {roots, replies, mentions} = getAncestorTags(event.tags)
 
-  // Root comes first
-  if (roots.length > 0) {
-    for (const t of roots) {
-      tags.push([...t.slice(0, 2), ctx.app.router.EventRoots(event).getUrl(), "root"])
+    // Root comes first
+    if (roots.length > 0) {
+      for (const t of roots) {
+        tags.push([...t.slice(0, 2), ctx.app.router.EventRoots(event).getUrl(), "root"])
+      }
+    } else {
+      for (const t of replies) {
+        tags.push([...t.slice(0, 2), ctx.app.router.EventParents(event).getUrl(), "root"])
+      }
     }
-  } else {
-    for (const t of replies) {
-      tags.push([...t.slice(0, 2), ctx.app.router.EventParents(event).getUrl(), "root"])
-    }
-  }
 
-  // Make sure we don't repeat any tag values
-  const isRepeated = (v: string) => tagValues.includes(v) || tags.find(t => t[1] === v)
+    // Make sure we don't repeat any tag values
+    const isRepeated = (v: string) => tagValues.includes(v) || tags.find(t => t[1] === v)
 
-  // Inherit mentions
-  for (const t of mentions) {
-    if (!isRepeated(t[1])) {
-      tags.push([...t.slice(0, 3), "mention"])
-    }
-  }
-
-  // Inherit replies if they weren't already included
-  if (roots.length > 0) {
-    for (const t of replies) {
+    // Inherit mentions
+    for (const t of mentions) {
       if (!isRepeated(t[1])) {
         tags.push([...t.slice(0, 3), "mention"])
       }
     }
-  }
 
-  // Add a/e-tags for the event event
-  for (const t of tagEvent(event, replies.length > 0 ? "reply" : "root")) {
-    tags.push(t)
+    // Inherit replies if they weren't already included
+    if (roots.length > 0) {
+      for (const t of replies) {
+        if (!isRepeated(t[1])) {
+          tags.push([...t.slice(0, 3), "mention"])
+        }
+      }
+    }
+
+    // Add a/e-tags for the event event
+    for (const t of tagEvent(event, replies.length > 0 ? "reply" : "root")) {
+      tags.push(t)
+    }
+  } else {
+    const {roots} = getAncestorTags(event.tags)
+    if (roots.length > 0) {
+      for (const t of roots) {
+        tags.push([...t.slice(0, 2), ctx.app.router.EventRoots(event).getUrl()])
+      }
+    } else {
+      if (isReplaceableKind(event.kind)) {
+        tags.push(["A", getAddress(event), ctx.app.router.EventRoots(event).getUrl()])
+      }
+      tags.push(["E", event.id, ctx.app.router.EventRoots(event).getUrl()])
+      tags.push(["P", event.pubkey, ctx.app.router.EventRoots(event).getUrl()])
+      tags.push(["K", event.kind.toString(), ctx.app.router.EventRoots(event).getUrl()])
+    }
+    if (isReplaceableKind(event.kind)) {
+      tags.push(["a", getAddress(event), ctx.app.router.EventParents(event).getUrl()])
+    }
+    tags.push(["e", event.id, ctx.app.router.EventParents(event).getUrl()])
+    tags.push(["p", event.pubkey, ctx.app.router.EventParents(event).getUrl()])
+    tags.push(["k", event.kind.toString(), ctx.app.router.EventParents(event).getUrl()])
   }
 
   return tags
