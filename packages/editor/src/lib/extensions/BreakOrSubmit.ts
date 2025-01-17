d636abb0a6ca7be40c6cff00994846d3e234afe7
Jon Staab <shtaab@gmail.com>
2025-01-17 14:31:42 -0800

    Bring back aggression

--- packages/editor/src/lib/extensions/BreakOrSubmit.ts
+++ packages/editor/src/lib/extensions/BreakOrSubmit.ts
@@ -3,6 +3,9 @@ import {HardBreak, type HardBreakOptions} from "@tiptap/extension-hard-break"
 export interface BreakOrSubmitOptions extends HardBreakOptions {
   /** Handler for when enter is pressed. */
   submit: () => void
+
+  /** Whether to call `submit` on unmodified Enter */
+  aggressive?: boolean
 }
 
 export const BreakOrSubmit = HardBreak.extend<BreakOrSubmitOptions>({
@@ -13,6 +16,15 @@ export const BreakOrSubmit = HardBreak.extend<BreakOrSubmitOptions>({
         this.options.submit()
 
         return true
+      },
+      "Enter": () => {
+        if (this.options.aggressive) {
+          this.options.submit()
+
+          return true
+        }
+
+        return false
       },
     }
   },
