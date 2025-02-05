29586db825358c37bbea969688d5d854be552841
Ticruz <thib.duchene@gmail.com>
2025-02-05 17:16:23 +0100

    configure vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from "vitest/config"
 
 export default defineConfig({
   test: {
-    environment: "node",
+    environment: "happy-dom",
     include: ["packages/**/*.test.ts"],
     coverage: {
       provider: "v8",
