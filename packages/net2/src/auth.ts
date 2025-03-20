7d0d303daebd56487996ce9ee670ab1ff31e49ab
Jon Staab <shtaab@gmail.com>
2025-03-20 14:22:40 -0700

    Add task queue, work on socket

--- packages/net2/src/auth.ts
+++ packages/net2/src/auth.ts
@@ -1,11 +1,9 @@
-import type {WebSocketSubject} from "rxjs/websocket"
-import {Subject} from "rxjs"
 import type {SignedEvent} from "@welshman/util"
-import {createEvent, CLIENT_AUTH} from "@welshman/util"
-import type {SocketResponse} from "./socket.js"
+import {makeEvent, CLIENT_AUTH} from "@welshman/util"
+import type {ISocket} from "./socket.js"
 
-export const createAuthEvent = (url: string, challenge: string) =>
-  createEvent(CLIENT_AUTH, {
+export const makeAuthEvent = (url: string, challenge: string) =>
+  makeEvent(CLIENT_AUTH, {
     tags: [
       ["relay", url],
       ["challenge", challenge],
@@ -17,22 +15,11 @@ export type AuthResult = {
   reason?: string
 }
 
-export const authenticate = (socket: WebSocketSubject<SocketResponse>, event: SignedEvent) => {
-  const subject = new Subject<AuthResult>()
+export const authenticate = (socket: ISocket, event: SignedEvent) =>
+  new Promise(resolve => {
+    socket.send(["AUTH", event])
 
-  socket.next(["AUTH", event])
-
-  socket.subscribe(message => {
-    if (message[0] === "OK") {
-      const [id, ok = false, reason = ""] = message.slice(1)
-
-      if (id === event.id) {
-        subject.next({ok, reason})
-      }
-    }
+    socket.onOk(([id, ok = false, reason = ""]) => {
+      if (id === event.id) resolve({ok, reason})
+    })
   })
-
-  return subject
-}
-
-export const forceAuth = <T>(socket: WebSocketSubject<T>) => {}
---
+++ packages/net2/src/message.ts
@@ -0,0 +1,38 @@
+import type {SignedEvent} from "@welshman/util"
+
+export enum RelayMessageType {
+  Auth = "AUTH",
+  Event = "EVENT",
+  Eose = "EOSE",
+  Ok = "OK",
+}
+
+export type RelayAuthPayload = [string]
+
+export type RelayEventPayload = [string, SignedEvent]
+
+export type RelayEosePayload = [string, SignedEvent]
+
+export type RelayOkPayload = [string, boolean, string]
+
+export type RelayAuthMessage = ["AUTH", ...RelayAuthPayload]
+
+export type RelayEventMessage = ["EVENT", ...RelayEventPayload]
+
+export type RelayEoseMessage = ["EOSE", ...RelayEosePayload]
+
+export type RelayOkMessage = ["OK", ...RelayOkPayload]
+
+export type RelayMessage = any[]
+
+export const isRelayAuthMessage = (m: RelayMessage): m is RelayAuthMessage =>
+  m[0] === RelayMessageType.Auth
+
+export const isRelayEventMessage = (m: RelayMessage): m is RelayEventMessage =>
+  m[0] === RelayMessageType.Event
+
+export const isRelayEoseMessage = (m: RelayMessage): m is RelayEoseMessage =>
+  m[0] === RelayMessageType.Eose
+
+export const isRelayOkMessage = (m: RelayMessage): m is RelayOkMessage =>
+  m[0] === RelayMessageType.Ok
