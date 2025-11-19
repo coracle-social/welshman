d54923e8f060b6a339ef1234e100a1d13eec09f9
Jon Staab <shtaab@gmail.com>
2025-11-19 14:30:54 -0800

    Re-work repository derivation

--- packages/store/src/repository.ts
+++ packages/store/src/repository.ts
@@ -1,5 +1,10 @@
-import {derived} from "svelte/store"
+import {derived, readable, Readable} from "svelte/store"
 import {
+  on,
+  indexBy,
+  mapPop,
+  Maybe,
+  call,
   sortBy,
   identity,
   ensurePlural,
@@ -9,164 +14,286 @@ import {
   first,
 } from "@welshman/lib"
 import {matchFilters, getIdAndAddress, getIdFilters, Filter, TrustedEvent} from "@welshman/util"
-import {Repository} from "@welshman/net"
-import {custom} from "./custom.js"
+import {Repository, RepositoryUpdate, Tracker} from "@welshman/net"
+import {deriveDeduplicated} from './memoize.js'
 
-export type DeriveEventsMappedOptions<T> = {
+// Events by id
+
+export type EventsById = Map<string, TrustedEvent>
+
+export type DeriveEventsByIdOptions = {
   filters: Filter[]
-  eventToItem: (event: TrustedEvent) => undefined | T | T[] | Promise<undefined | T | T[]>
-  itemToEvent: (item: T) => TrustedEvent
-  throttle?: number
+  repository: Repository
   includeDeleted?: boolean
 }
 
-export const deriveEventsMapped = <T>(
-  repository: Repository,
-  {
-    filters,
-    eventToItem,
-    itemToEvent,
-    throttle = 0,
-    includeDeleted = false,
-  }: DeriveEventsMappedOptions<T>,
-) =>
-  custom<T[]>(
-    setter => {
-      let data: T[] = []
-      const deferred = new Set()
+export const deriveEventsById = ({
+  filters,
+  repository,
+  includeDeleted,
+}: DeriveEventsByIdOptions) => {
+  const eventsById: EventsById = indexBy(e => e.id, repository.query(filters, {includeDeleted}))
 
-      const defer = (event: TrustedEvent, promise: Promise<undefined | T | T[]>) => {
-        deferred.add(event.id)
+  return readable(eventsById, set => {
+    return on(repository, "update", ({added, removed}: RepositoryUpdate) => {
+      let dirty = false
 
-        void promise.then(items => {
-          if (deferred.has(event.id)) {
-            deferred.delete(event.id)
-
-            for (const item of removeUndefined(ensurePlural(items))) {
-              data.push(item)
-            }
-
-            setter(sortBy(item => -itemToEvent(item).created_at, data))
-          }
-        })
-      }
-
-      for (const event of repository.query(filters, {includeDeleted})) {
-        const items = eventToItem(event)
-
-        if (!items) {
-          continue
-        }
-
-        if (items instanceof Promise) {
-          defer(event, items)
-        } else {
-          for (const item of removeUndefined(ensurePlural(items))) {
-            data.push(item)
-          }
+      for (const event of added) {
+        if (matchFilters(filters, event)) {
+          dirty = true
+          eventsById.set(event.id, event)
         }
       }
 
-      setter(sortBy(item => -itemToEvent(item).created_at, data))
-
-      const onUpdate = batch(300, (updates: {added: TrustedEvent[]; removed: Set<string>}[]) => {
-        const removed = new Set()
-        const added = new Map()
-
-        // Apply updates in order
-        for (const update of updates) {
-          for (const event of update.added.values()) {
-            added.set(event.id, event)
-            removed.delete(event.id)
-          }
-
-          for (const id of update.removed) {
-            removed.add(id)
-            added.delete(id)
-            deferred.delete(id)
-          }
+      for (const id of removed) {
+        if (mapPop(id, eventsById)) {
+          dirty = true
         }
+      }
 
+      if (dirty) {
+        set(eventsById)
+      }
+    })
+  })
+}
+
+export const deriveEvents = (eventsByIdStore: Readable<EventsById>) =>
+  deriveDeduplicated(eventsByIdStore, eventsById => Array.from(eventsById.values()))
+
+export const deriveEventsAsc = (eventsStore: Readable<TrustedEvent[]>) =>
+  deriveDeduplicated(eventsStore, events => sortBy(e => e.created_at, events))
+
+export const deriveEventsDesc = (eventsStore: Readable<TrustedEvent[]>) =>
+  deriveDeduplicated(eventsStore, events => sortBy(e => -e.created_at, events))
+
+// Events by id by url
+
+export type EventsByIdByUrl = Map<string, EventsById>
+
+export type DeriveEventsByIdByUrlOptions = DeriveEventsByIdOptions & {
+  tracker: Tracker
+}
+
+export const deriveEventsByIdByUrl = ({
+  filters,
+  tracker,
+  repository,
+  includeDeleted,
+}: DeriveEventsByIdByUrlOptions) => {
+  const eventsByIdByUrl: EventsByIdByUrl = new Map()
+
+  const addEvent = (url: string, event: TrustedEvent) => {
+    const eventsById = eventsByIdByUrl.get(url)
+
+    if (!eventsById?.has(event.id)) {
+      // Create a new map so we can detect which key changed
+      const newEventsById = new Map(eventsById)
+
+      newEventsById.set(event.id, event)
+      eventsByIdByUrl.set(url, newEventsById)
+
+      return true
+    }
+
+    return false
+  }
+
+  const removeEvent = (url: string, id: string) => {
+    const eventsById = eventsByIdByUrl.get(url)
+
+    if (eventsById?.has(id)) {
+      eventsById.delete(id)
+
+      if (eventsById.size === 0) {
+        eventsByIdByUrl.delete(url)
+      } else {
+        // Create a new map so we can detect which key changed
+        eventsByIdByUrl.set(url, new Map(eventsById))
+      }
+
+      return true
+    }
+
+    return false
+  }
+
+  for (const event of repository.query(filters, {includeDeleted})) {
+    for (const url of tracker.getRelays(event.id)) {
+      addEvent(url, event)
+    }
+  }
+
+  return readable(eventsByIdByUrl, set => {
+    const unsubscribers = [
+      on(repository, "update", ({added, removed}: RepositoryUpdate) => {
         let dirty = false
-        for (const event of added.values()) {
-          if (matchFilters(filters, event)) {
-            const items = eventToItem(event)
 
-            if (items instanceof Promise) {
-              defer(event, items)
-            } else if (items) {
-              dirty = true
-
-              for (const item of removeUndefined(ensurePlural(items))) {
-                data.push(item as T)
-              }
-            }
+        for (const event of added) {
+          for (const url of tracker.getRelays(event.id)) {
+            dirty = dirty || addEvent(url, event)
           }
         }
 
-        if (!includeDeleted && removed.size > 0) {
-          const [deleted, ok] = partition(
-            (item: T) => getIdAndAddress(itemToEvent(item)).some((id: string) => removed.has(id)),
-            data,
-          )
-
-          if (deleted.length > 0) {
-            dirty = true
-            data = ok
+        for (const id of removed) {
+          for (const url of tracker.getRelays(id)) {
+            dirty = dirty || removeEvent(url, id)
           }
         }
 
         if (dirty) {
-          setter(sortBy(item => -itemToEvent(item).created_at, data))
+          set(eventsByIdByUrl)
         }
-      })
+      }),
+      on(tracker, "add", (id: string, url: string) => {
+        const event = repository.getEvent(id)
 
-      repository.on("update", onUpdate)
+        if (event && addEvent(url, event)) {
+          set(eventsByIdByUrl)
+        }
+      }),
+      on(tracker, "remove", (id: string, url: string) => {
+        if (removeEvent(url, id)) {
+          set(eventsByIdByUrl)
+        }
+      }),
+      on(tracker, "load", () => {
+        eventsByIdByUrl.clear()
 
-      return () => repository.off("update", onUpdate)
-    },
-    {throttle},
-  )
+        for (const event of repository.query(filters, {includeDeleted})) {
+          for (const url of tracker.getRelays(event.id)) {
+            addEvent(url, event)
+          }
+        }
 
-export type DeriveEventsOptions<T> = Omit<
-  DeriveEventsMappedOptions<T>,
-  "itemToEvent" | "eventToItem"
->
+        set(eventsByIdByUrl)
+      }),
+      on(tracker, "clear", () => {
+        eventsByIdByUrl.clear()
 
-export const deriveEvents = <T>(repository: Repository, opts: DeriveEventsOptions<T>) =>
-  deriveEventsMapped<TrustedEvent>(repository, {
-    ...opts,
-    eventToItem: identity,
-    itemToEvent: identity,
+        set(eventsByIdByUrl)
+      }),
+    ]
+
+    return () => unsubscribers.forEach(call)
   })
+}
+
+export const deriveEventsByIdForUrl = (url: string, eventsByIdByUrlStore: Readable<EventsByIdByUrl>) =>
+  deriveDeduplicated(eventsByIdByUrlStore, eventsByIdByUrl => eventsByIdByUrl.get(url))
+
+
+// Items by key
+
+export type ItemsByKey<T> = Map<string, T>
+
+export type EventToItem<T> = (event: TrustedEvent) => T
+
+export type DeriveItemsByKeyOptions<T> = {
+  getKey: (item: T) => string
+  filters: Filter[]
+  repository: Repository
+  eventToItem: EventToItem<T>
+  includeDeleted?: boolean
+}
+
+export const deriveItemsByKey = <T>({
+  getKey,
+  filters,
+  repository,
+  eventToItem,
+  includeDeleted,
+}: DeriveItemsByKeyOptions<T>) => {
+  const deferred = new Map<string, Promise<Maybe<T>>>()
+  const itemsByKey = new Map<string, T>()
+  const idsByKey = new Map<string, string>()
+  const keysById = new Map<string, string>()
+
+  return readable(itemsByKey, set => {
+    const addEvent = async (event: TrustedEvent) => {
+      if (deferred.has(event.id)) return
+      if (keysById.has(event.id)) return
+
+      const itemOrPromise = eventToItem(event)
+
+      if (itemOrPromise instanceof Promise) {
+        deferred.set(event.id, itemOrPromise)
+      }
+
+      const item = await itemOrPromise
+
+      deferred.delete(event.id)
+
+      if (item) {
+        const key = getKey(item)
+
+        itemsByKey.set(key, item)
+        idsByKey.set(key, event.id)
+        keysById.set(event.id, key)
+
+        set(itemsByKey)
+      }
+    }
+
+    for (const event of repository.query(filters, {includeDeleted})) {
+      addEvent(event)
+    }
+
+    return on(repository, "update", ({added, removed}: RepositoryUpdate) => {
+      let dirty = false
+
+      for (const event of added) {
+        if (matchFilters(filters, event)) {
+          addEvent(event)
+          dirty = true
+        }
+      }
+
+      if (!includeDeleted) {
+        for (const id of removed) {
+          const key = mapPop(id, keysById)
+
+          if (key) {
+            idsByKey.delete(key)
+            itemsByKey.delete(key)
+            dirty = true
+          }
+        }
+      }
+
+      if (dirty) {
+        set(itemsByKey)
+      }
+    })
+  })
+}
+
+export const deriveItems = <T>(itemsByKeyStore: Readable<ItemsByKey<T>>) =>
+  deriveDeduplicated(itemsByKeyStore, itemsByKey => Array.from(itemsByKey.values()))
+
+export const deriveItemsSorted = <T>(sortFn: (item: T) => number, itemsStore: Readable<T[]>) =>
+  deriveDeduplicated(itemsStore, items => sortBy(sortFn, items))
+
+// Miscellaneous other stuff
 
 export const deriveEvent = (repository: Repository, idOrAddress: string) =>
   derived(
-    deriveEvents(repository, {
+    deriveEventsById({
+      repository,
       filters: getIdFilters([idOrAddress]),
       includeDeleted: true,
     }),
-    first,
+    $m => first($m.values()),
   )
 
 export const deriveIsDeleted = (repository: Repository, event: TrustedEvent) =>
-  custom<boolean>(setter => {
-    setter(repository.isDeleted(event))
+  readable(repository.isDeleted(event), set => {
+    const unsubscribe = on(repository, 'update', ({removed}: RepositoryUpdate) => {
+      if (removed.has(event.id)) {
+        set(true)
+        unsubscribe()
+      }
+    })
 
-    const onUpdate = batch(300, () => setter(repository.isDeleted(event)))
-
-    repository.on("update", onUpdate)
-
-    return () => repository.off("update", onUpdate)
-  })
-
-export const deriveIsDeletedByAddress = (repository: Repository, event: TrustedEvent) =>
-  custom<boolean>(setter => {
-    setter(repository.isDeletedByAddress(event))
-
-    const onUpdate = batch(300, () => setter(repository.isDeletedByAddress(event)))
-
-    repository.on("update", onUpdate)
-
-    return () => repository.off("update", onUpdate)
+    return unsubscribe
   })
