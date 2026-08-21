878b67160a078a5ddcfee32ce020db39f9dd8d8b
Jon Staab <jstaab@protonmail.com>
2026-08-21 09:22:14 -0700

    Protect relay leave/join events

--- packages/domain/src/kinds/RelayLeave.ts
+++ packages/domain/src/kinds/RelayLeave.ts
@@ -2,12 +2,19 @@ import {RELAY_LEAVE} from "@welshman/util"
 import {EventReader} from "../core/EventReader.js"
 import {EventWriter} from "../core/EventWriter.js"
 import {KindFactory} from "../core/Kind.js"
+import type {KindContext} from "../core/Kind.js"
 
 // Ephemeral kind-28936 relay/space leave marker.
 export class RelayLeaveReader extends EventReader {}
 
 export class RelayLeaveWriter extends EventWriter<RelayLeaveReader> {
   readonly requiresRelays = true
+
+  constructor(kind: number, context: KindContext, reader?: RelayLeaveReader) {
+    super(kind, context, reader)
+
+    this.setProtected(true)
+  }
 }
 
 export const RelayLeave = new KindFactory({
