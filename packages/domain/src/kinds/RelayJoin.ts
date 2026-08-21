4e86586e610d4760937ef2c248e2a5d555f7e410
Jon Staab <jstaab@protonmail.com>
2026-08-21 09:22:14 -0700

    Protect relay leave/join events

--- packages/domain/src/kinds/RelayJoin.ts
+++ packages/domain/src/kinds/RelayJoin.ts
@@ -3,6 +3,7 @@ import {RELAY_JOIN, tagSpec, tagValue} from "@welshman/util"
 import {EventReader} from "../core/EventReader.js"
 import {EventWriter} from "../core/EventWriter.js"
 import {KindFactory} from "../core/Kind.js"
+import type {KindContext} from "../core/Kind.js"
 
 // Ephemeral kind-28934 relay/space join request.
 export class RelayJoinReader extends EventReader {
@@ -17,6 +18,12 @@ export class RelayJoinReader extends EventReader {
 
 export class RelayJoinWriter extends EventWriter<RelayJoinReader> {
   readonly requiresRelays = true
+
+  constructor(kind: number, context: KindContext, reader?: RelayLeaveReader) {
+    super(kind, context, reader)
+
+    this.setProtected(true)
+  }
 
   setClaim(claim: string) {
     return this.dropTags(spec(["claim"])).addTags(["claim", claim])
